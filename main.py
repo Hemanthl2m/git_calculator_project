c74ef74981d623bdd3a523500c517add8a656a63
Hemanth <hemanth@hemanth.l2m.internal>
2026-09-16 17:07:33 +0530

    changed the average feauture and added

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from operations import add, subtract, multiply, divide
+from operations import add, subtract, multiply, divide, average
 
 def main():
     print("Simple Calculator")
@@ -9,6 +9,7 @@ def main():
     print("Subtract:", subtract(a, b))
     print("Multiply:", multiply(a, b))
     print("Division:", divide(a,b))
+    print("Average: ", average(a,b))
 
 if __name__ == "__main__":
     main()
